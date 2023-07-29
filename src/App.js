c7fe4d571187bef1a398c1647db66252ec8fc743
bernardfernando <jagathf@gmail.com>
2023-07-29 02:42:10 +0100

    amends

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [searchQuery, setsearchQuery] = useState("");
+  const [location, setlocation] = useState({});
+  const [map, setMap] = useState("");
+
+  function handleSearch(event) {
+    //console.log("location found")
+    setsearchQuery(event.target.value);
+    console.log(searchQuery);
+  }
+
+  async function getLocation(event) {
+    try {
+      event.preventDefault();
+      event.target.input.value = "";
+      const API = `GET https://eu1.locationiq.com/v1/search?key=${process.env.REACT_APP_API_KEY}&q=SEARCH_STRING&format=json`;
+      console.log(API);
+      const res = await axios.get(API);
+      setlocation(res.data[0]);
+      handleMap(res.data[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  function handleMapData(data) {
+    const API = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${data.lat},${data.lon}&zoom=9`;
+    setMap(API);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getLocation}>
+        <input
+          type="text"
+          placeholder="search for city"
+          onChange={handleSearch}
+          name="input"
+        />
+        <button className="butt1" type="submit">
+          Explore
+        </button>
+      </form>
     </div>
   );
 }
